8fd01fbfc1cdfa92fd907912f305c3f4070269e5
Sofiag8 <1943@holbertonschool.com>
2020-10-29 17:34:58 +0000

    tested with pep8 task 6 working

---
+++ console.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import cmd
+""" The cmd module is mainly useful for building custom shells
+that let a user work with a program interactively. """
+
+
+class HBNBCommand(cmd.Cmd):
+    """ entry point of the command interpreter """
+    prompt = '(hbnb) '
+
+    def do_EOF(self, arg):
+        """ EOF to exit the program """
+        return True
+
+    def do_quit(self, arg):
+        """ quit command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """ method called when an empty line is entered in
+        response to the prompt.
+        onecmd(str): Interpret the argument as though it had been
+                     typed in response to the prompt.
+        onecmd help us to implement an empty line + ENTER
+        shouldnt execute anything
+        """
+        if self.lastcmd == "":
+            return self.onecmd('\n')
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
