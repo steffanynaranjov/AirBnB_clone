cc35ac329ba1638dc5b2383d3122ffb4ceb6dee7
Sofiag8 <1943@holbertonschool.com>
2020-10-29 21:14:08 +0000

    Docstring

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """
 module for init FileStorage
+To create a unique FileStorage instance for the application improting
+FileStorage, creating the variable storage (as instance of FileStorage)
+and callign reaload() method on this variable
 """
 from models.engine.file_storage import FileStorage
 
--- models/base_model.py
+++ models/base_model.py
@@ -9,12 +9,28 @@ import models
 
 class BaseModel:
     """
-    first class base
+    BaseModel that defines all common attributes/methods for other classes
+
+    PUBLIC INSTANCE ATTRIBUTES:
+    id: string - assign with an uuid when an instance is created
+         uuid.uuid4(): generate a unique id but cant forget to
+         convert to string. The goal is to have a unique id for each BaseModel
+
+    created_at:  datetime - assign with the current datetime when an instance
+                 is created
+
+    updated_at: datetime - assign with the current datetime when an instance
+                is created and it will be updated every time you change your
+                object
+
+    __str__: should print: [<class name>] (<self.id>) <self.__dict__>
+
+    PUBLIC INSTANCE METHODS
+    save(self):
+    to_dict(self):
     """
     def __init__(self, *args, **kwargs):
-        """
-        starting the method
-        """
+        """ initialization """
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
@@ -30,7 +46,7 @@ class BaseModel:
 
     def __str__(self):
         """
-        format to print str
+        __str__ method should print: [<class name>] (<self.id>) <self.__dict__>
         """
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id,
@@ -38,7 +54,8 @@ class BaseModel:
 
     def save(self):
         """
-        a update for the date method
+        save method  updates the public instance attribute updated_at
+        with the current datetim
         """
         self.updated_at = datetime.now()
         models.storage.new(self)
@@ -46,7 +63,20 @@ class BaseModel:
 
     def to_dict(self):
         """
-        returns a dictionary
+        This method will be the first piece of the serialization/
+        deserialization process: create a dictionary representation
+        with simple object type of our BaseModel.
+
+        By using self.__dict__, only instance attributes set will be returned.
+        A key __class__ must be added to this dictionary with the class name
+        of the object.
+        created_at and updated_at must be converted to string object in
+        ISO format.
+
+        Format: %Y-%m-%dT%H:%M:%S.%f (ex: 2017-06-14T22:31:03.285259)
+
+        Returns: a dictionary containing all keys/values of __dict__
+                 of the instance.
         """
         new_dict = dict(self.__dict__)
         new_dict["__class__"] = type(self).__name__
