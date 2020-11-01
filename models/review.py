3205e5a082e84f8116178988703f1c6bad4d1ba6
steffanynaranjov <steffany.naranjo@gmail.com>
2020-11-01 17:08:19 +0000

    class review

---
+++ models/review.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+class review
+"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """
+    class review
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
