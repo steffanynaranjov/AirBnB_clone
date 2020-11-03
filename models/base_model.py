77d86cb4c3f2b50444a9916a548704cc5d6467d3
steffanynaranjov <steffany.naranjo@gmail.com>
2020-11-03 22:48:55 +0000

    base model change

--- models/base_model.py
+++ models/base_model.py
@@ -28,10 +28,12 @@ class BaseModel:
         """ initialization """
         if kwargs:
             for key, value in kwargs.items():
-                if key != "__class__":
-                    if key in ["created_at", "updated_at"]:
-                        value = datetime.strptime(value,
-                                                  "%Y-%m-%dT%H:%M:%S.%f")
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key,
+                            datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key == "__class__":
+                    setattr(self, key, type(self))
+                else:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
