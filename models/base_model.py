f54022aac503ed4d52c6807bc22df636ce19db96
steffanynaranjov <steffany.naranjo@gmail.com>
2020-11-03 20:29:11 +0000

    add base model

--- models/base_model.py
+++ models/base_model.py
@@ -10,40 +10,29 @@ import models
 class BaseModel:
     """
     BaseModel that defines all common attributes/methods for other classes
-
     PUBLIC INSTANCE ATTRIBUTES:
     id: string - assign with an uuid when an instance is created
          uuid.uuid4(): generate a unique id but cant forget to
          convert to string. The goal is to have a unique id for each BaseModel
-
     created_at:  datetime - assign with the current datetime when an instance
                  is created
-
     updated_at: datetime - assign with the current datetime when an instance
                 is created and it will be updated every time you change your
                 object
-
     __str__: should print: [<class name>] (<self.id>) <self.__dict__>
-
     PUBLIC INSTANCE METHODS
     save(self):
     to_dict(self):
     """
     def __init__(self, *args, **kwargs):
         """ initialization """
-        if (kwargs):
+        if kwargs:
             for key, value in kwargs.items():
-                if (key == "__class__"):
-                    continue
-                elif (key == "updated_at" or key == "created_at"):
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if "id" not in kwargs.keys():
-                    self.id = str(uuid4())
-                if "created_at" not in kwargs.keys():
-                    self.created_at = datetime.now()
-                if "updated_at" not in kwargs.keys():
-                    self.updated_at = datetime.now()
-                setattr(self, key, value)
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        value = datetime.strptime(value,
+                                                  "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -72,15 +61,12 @@ class BaseModel:
         This method will be the first piece of the serialization/
         deserialization process: create a dictionary representation
         with simple object type of our BaseModel.
-
         By using self.__dict__, only instance attributes set will be returned.
         A key __class__ must be added to this dictionary with the class name
         of the object.
         created_at and updated_at must be converted to string object in
         ISO format.
-
         Format: %Y-%m-%dT%H:%M:%S.%f (ex: 2017-06-14T22:31:03.285259)
-
         Returns: a dictionary containing all keys/values of __dict__
                  of the instance.
         """
