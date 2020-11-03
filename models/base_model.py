fcf902e5e052d6a68f499a9eda62891783030804
steffanynaranjov <steffany.naranjo@gmail.com>
2020-11-03 20:18:19 +0000

    uptate init base model

--- models/base_model.py
+++ models/base_model.py
@@ -30,19 +30,20 @@ class BaseModel:
     to_dict(self):
     """
     def __init__(self, *args, **kwargs):
-        """ initialization """
-        if kwargs:
+        """ Initialization """
+        if (kwargs):
             for key, value in kwargs.items():
-                if key != "__class__":
-                    if key in ["created_at", "updated_at"]:
-                        value = datetime.strptime(value,
-                                                  "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
+                if (key == '__class__'):
+                    continue
+                elif (key == 'updated_at' or key == 'created_at'):
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if 'id' not in kwargs.keys():
+                    self.id = str(uuid4())
+                if 'created_at' not in kwargs.keys():
+                    self.created_at = datetime.now()
+                if 'updated_at' not in kwargs.keys():
+                    self.updated_at = datetime.now()
+                setattr(self, key, value)
 
     def __str__(self):
         """
