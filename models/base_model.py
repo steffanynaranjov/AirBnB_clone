2040912ef63166fd5a0b8db719e64aead51e37bc
Sofiag8 <1943@holbertonschool.com>
2020-11-03 19:09:09 +0000

    not condition

--- models/base_model.py
+++ models/base_model.py
@@ -31,7 +31,7 @@ class BaseModel:
     """
     def __init__(self, *args, **kwargs):
         """ initialization """
-        if kwargs is not None and Kwargs != {}:
+        if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
                     if key in ["created_at", "updated_at"]:
