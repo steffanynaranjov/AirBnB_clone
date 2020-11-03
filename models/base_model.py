cf1938a98ec72d9f71de2c21832a24b0838d43cd
steffanynaranjov <steffany.naranjo@gmail.com>
2020-11-03 20:27:19 +0000

    try

--- models/base_model.py
+++ models/base_model.py
@@ -30,10 +30,10 @@ class BaseModel:
     to_dict(self):
     """
     def __init__(self, *args, **kwargs):
-        """ Initialization """
-        if kwargs:
+        """ initialization """
+        if (kwargs):
             for key, value in kwargs.items():
-                if (key == '__class__'):
+                if (key == "__class__"):
                     continue
                 elif (key == "updated_at" or key == "created_at"):
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
@@ -44,11 +44,10 @@ class BaseModel:
                 if "updated_at" not in kwargs.keys():
                     self.updated_at = datetime.now()
                 setattr(self, key, value)
-
         else:
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
