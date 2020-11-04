b3f5187de05f0774b6c64f36db73e20126dbf007
Sofiag8 <1943@holbertonschool.com>
2020-11-04 03:16:14 +0000

    fixing

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -12,13 +12,10 @@ class TestFileStorage(unittest.TestCase):
     """ The unittest module provides a rich set of tools for
     constructing and running tests"""
 
-    def setUp(self):
-        """ set up test1 as FileStorage instance """
-        self.test1 = FileStorage()
-
     def test_instance(self):
         """ if is instance of FileStorage """
-        self.assertIsInstance(self.test1, FileStorage)
+        test1 = FileStorage()
+        self.assertIsInstance(test1, FileStorage)
 
     def test_docstring_mandatory(self):
         """ docstring at the module are necesary and good practice
@@ -27,9 +24,6 @@ class TestFileStorage(unittest.TestCase):
         """
         self.assertIsNotNone(models.engine.file_storage.__doc__)
         self.assertIsNotNone(FileStorage.__doc__)
-        self.assertIsNotNone(FileStorage.__init__.__doc__)
-        self.assertIsNotNone(FileStorage.save.__doc__)
-        self.assertIsNotNone(FileStorage.new.__doc__)
 
     def test_permissions(self):
         """ test files are with execution, read, write and existence permission
